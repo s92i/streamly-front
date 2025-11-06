a0c55fe9e223f7337d6ce3e9279bd2f42c22e6c6
s92i <sofiane.92izii@gmail.com>
2025-11-06 01:41:02 +0100

    Added Graphql queries and Shadcn

---
+++ src/components/ui/common/Button.tsx
@@ -0,0 +1,50 @@
+import { Slot } from '@radix-ui/react-slot'
+import { type VariantProps, cva } from 'class-variance-authority'
+import { ComponentProps } from 'react'
+
+import { cn } from '@/utils/tw-merge'
+
+const buttonVariants = cva(
+	"inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+	{
+		variants: {
+			variant: {
+				default: 'bg-primary text-primary-foreground',
+				outline: 'border border-border bg-background',
+				secondary: 'bg-secondary text-secondary-foreground',
+				ghost: 'text-accent-foreground hover:bg-accent hover:text-accent-foreground'
+			},
+			size: {
+				default: 'h-10 px-5 py-2 rounded-full',
+				icon: 'size-10 rounded-full'
+			}
+		},
+		defaultVariants: {
+			variant: 'default',
+			size: 'default'
+		}
+	}
+)
+
+function Button({
+	className,
+	variant,
+	size,
+	asChild = false,
+	...props
+}: ComponentProps<'button'> &
+	VariantProps<typeof buttonVariants> & {
+		asChild?: boolean
+	}) {
+	const Comp = asChild ? Slot : 'button'
+
+	return (
+		<Comp
+			data-slot='button'
+			className={cn(buttonVariants({ variant, size, className }))}
+			{...props}
+		/>
+	)
+}
+
+export { Button, buttonVariants }
---
+++ src/libs/i18n/config.ts
@@ -0,0 +1,4 @@
+export const COOKIE_NAME = 'language'
+export const languages = ['en', 'fr'] as const
+export const defaultLanguage: Language = 'en'
+export type Language = (typeof languages)[number]
