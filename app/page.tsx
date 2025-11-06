d48bff0a3c0a5ba7636fe9f47360e7d58d9d9cb1
s92i <sofiane.92izii@gmail.com>
2025-11-06 20:10:31 +0100

    Fixed project design

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 
+import { Button } from "@/components/ui/common/Button";
 import { useTranslations } from "next-intl";
 
 export default function Home() {
   const t = useTranslations("home");
 
   return (
-    <div className="text-4xl font-bold text-blue-500">
-      {t("title")}
-      <h1 className="-px-10 text-3xl text-red-500">test</h1>
-    </div>
+    <>
+      <div className="text-4xl font-bold text-blue-500">{t("title")}</div>
+      <div>
+        <Button>Default</Button>
+        <Button variant="outline">Outline</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="ghost">Ghost</Button>
+      </div>
+    </>
   );
 }
